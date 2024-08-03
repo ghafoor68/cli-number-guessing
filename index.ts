6559004617c5b7faaee6ccdc378ca0975fa77cd4
ghafoor68 <a.ghafoor.112233@gmail.com>
2024-08-03 16:56:13 +0500

    Task submission

---
+++ index.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+// 1) Computer will generate a random number
+
+// 2) User input for guesing number
+
+// 3) Compare user input with computer generated number and show result
+
+const randomNumber =Math.floor(Math.random() * 6 + 1);
+// console.log(randomNumber);
+
+
+const answers = await inquirer.prompt(
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1-6 ",
+    },
+);
+
+if (answers.userGuessedNumber === randomNumber ) {
+    console.log("Congratulations! You guessed right number");
+} else {
+    console.log("You guessed wrong number");
+}
+
